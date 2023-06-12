319e5bfef80da2a2eec971afa177bd58a6211e2c
Bhavika2101 <63244205+Bhavika2101@users.noreply.github.com>
2023-06-12 18:04:02 +0530

    Update main.tf

--- single-web-server/main.tf
+++ single-web-server/main.tf
@@ -20,7 +20,7 @@ terraform {
 # ------------------------------------------------------------------------------
 
 provider "aws" {
-  region = "us-east-2"
+  region = "ap-south-1"
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -29,6 +29,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   # Ubuntu Server 18.04 LTS (HVM), SSD Volume Type in us-east-2
+  name                   = "grpc-testing"
   ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
